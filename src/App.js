c63b573c2635b5b81826e57acb9c08e72d8f9fab
Avelina Kim <avelina@gmail.com>
2019-12-27 23:46:36 -0500

    Change to Victory bar graph

--- src/App.js
+++ src/App.js
@@ -1,116 +1,25 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
-import * as d3 from 'd3'
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme } from 'victory'
 
 const data = [
-  { time: 'At open', items: 24 },
-  { time: '11:00 PM', items: 0 },
-  { time: '12:00 PM', items: 4 },
+  { quarter: 1, earnings: 13000 },
+  { quarter: 2, earnings: 16500 },
+  { quarter: 3, earnings: 14250 },
+  { quarter: 4, earnings: 19000 },
 ]
 
 class App extends React.Component {
-  drawLineChart(data) {
-    const svgWidth = 800
-    const svgHeight = 400
-
-    const svg = d3
-      .select('svg')
-      .attr('width', svgWidth)
-      .attr('height', svgHeight)
-
-    const margin = { top: 20, right: 20, bottom: 30, left: 50 }
-    const g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-
-    // Create x and y scale
-    const width = svgWidth - margin.left - margin.right
-    const height = svgHeight - margin.top - margin.bottom
-
-    const y = d3
-      .scaleLinear()
-      .domain(d3.extent(data, (d) => d.items))
-      .range([height, 0])
-
-    const x = d3
-      .scalePoint()
-      .domain(data.map((d) => d.time))
-      .range([0, width])
-
-    // Create line
-    const itemsLine = d3
-      .line()
-      .x((d) => x(d.time))
-      .y((d) => y(d.items))
-
-    // Append x and y axes to the group g
-    g.append('g')
-      .attr('transform', 'translate(0,' + height + ')')
-      .call(d3.axisBottom(x))
-      .selectAll('text')
-      .style('text-anchor', 'end')
-      .attr('dx', '-.8em')
-      .attr('dy', '.15em')
-      .attr('transform', 'rotate(-50)')
-
-    g.append('g')
-      .call(d3.axisLeft(y))
-      .append('text')
-      .attr('fill', '#000')
-      .attr('y', -40)
-      .attr('x', -180)
-      .attr('dy', '0.71em')
-      .attr('text-anchor', 'end')
-      .text('Items (count)')
-
-    // Append the line as a path to group
-    g.append('path')
-      .datum(data)
-      .attr('fill', 'none')
-      .attr('stroke', 'red')
-      .attr('stroke-linejoin', 'round')
-      .attr('stroke-linecap', 'round')
-      .attr('stroke-width', 1.5)
-      .attr('d', itemsLine)
-
-    // Append legend group to svg
-    const legend = svg
-      .append('g')
-      .attr('class', 'legend')
-      .attr('x', width - 65)
-      .attr('y', 25)
-      .attr('height', 100)
-      .attr('width', 100)
-
-    legend
-      .append('rect')
-      .attr('x', width - 65)
-      .attr('y', 25)
-      .attr('width', 10)
-      .attr('height', 10)
-      .attr('fill', 'red')
-
-    legend
-      .append('text')
-      .attr('x', width - 50)
-      .attr('y', 35)
-      .text('Items')
-  }
-  componentDidMount() {
-    this.drawLineChart(data)
-  }
   render() {
     return (
       <div className="App">
-        <svg id="lineChart"></svg>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
+        <h1>Victory Tutorial</h1>
+        <VictoryChart theme={VictoryTheme.material} domainPadding={30}>
+          <VictoryAxis tickValues={[1, 2, 3, 4]} tickFormat={['Quarter 1', 'Quarter 2', 'Quarter 3', 'Quarter 4']} />
+          <VictoryAxis dependentAxis tickFormat={(tick) => `$${tick / 1000}k`} />
+          <VictoryBar data={data} x="quarter" y="earnings" />
+        </VictoryChart>
       </div>
     )
   }
