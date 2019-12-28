18890a89539a89fe17f745b0a811f9f485414b62
Avelina Kim <avelina@gmail.com>
2019-12-28 13:24:03 -0500

    Set up salary graph for schools

--- src/App.js
+++ src/App.js
@@ -2,36 +2,27 @@ import React from 'react'
 import './App.css'
 import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryStack } from 'victory'
 
-const data = [
-  { quarter: 1, earnings: 13000 },
-  { quarter: 2, earnings: 16500 },
-  { quarter: 3, earnings: 14250 },
-  { quarter: 4, earnings: 19000 },
+const techSchool = [
+  { salaryRange: [0, 50000], percentage: 18.6 },
+  { salaryRange: [50000, 60000], percentage: 49.2 },
+  { salaryRange: [60000, 70000], percentage: 27.1 },
+  { salaryRange: [70000, 80000], percentage: 3.4 },
+  { salaryRange: [80000, 90000], percentage: 0.0 },
+  { salaryRange: [90000, 150000], percentage: 1.7 },
 ]
 
-const data2017 = [
-  { quarter: 1, earnings: 13000 },
-  { quarter: 2, earnings: 16500 },
-  { quarter: 3, earnings: 14250 },
-  { quarter: 4, earnings: 19000 },
+const codeSchool = [
+  { salaryRange: [0, 100000], percentage: 25.0 },
+  { salaryRange: [100000, 110000], percentage: 3.1 },
+  { salaryRange: [110000, 120000], percentage: 25.0 },
+  { salaryRange: [120000, 130000], percentage: 9.4 },
+  { salaryRange: [130000, 140000], percentage: 12.5 },
+  { salaryRange: [140000, 150000], percentage: 25.0 },
 ]
-const data2018 = [
-  { quarter: 1, earnings: 15000 },
-  { quarter: 2, earnings: 12500 },
-  { quarter: 3, earnings: 19500 },
-  { quarter: 4, earnings: 13000 },
-]
-const data2019 = [
-  { quarter: 1, earnings: 11500 },
-  { quarter: 2, earnings: 13250 },
-  { quarter: 3, earnings: 20000 },
-  { quarter: 4, earnings: 15500 },
-]
-const data2020 = [
-  { quarter: 1, earnings: 18000 },
-  { quarter: 2, earnings: 13250 },
-  { quarter: 3, earnings: 15000 },
-  { quarter: 4, earnings: 12000 },
+
+const schoolsData = [
+  { name: 'codeSchool', school: codeSchool },
+  { name: 'techSchool', school: techSchool },
 ]
 
 class App extends React.Component {
@@ -39,16 +30,51 @@ class App extends React.Component {
     return (
       <div className="App">
         <h1>Victory Tutorial</h1>
-        <VictoryChart theme={VictoryTheme.material} domainPadding={30}>
-          <VictoryAxis tickValues={[1, 2, 3, 4]} tickFormat={['Quarter 1', 'Quarter 2', 'Quarter 3', 'Quarter 4']} />
-          <VictoryAxis dependentAxis tickFormat={(tick) => `$${tick / 1000}k`} />
-          <VictoryStack colorScale="qualitative">
-            <VictoryBar data={data2017} x="quarter" y="earnings" />
-            <VictoryBar data={data2018} x="quarter" y="earnings" />
-            <VictoryBar data={data2019} x="quarter" y="earnings" />
-            <VictoryBar data={data2020} x="quarter" y="earnings" />
-          </VictoryStack>
-        </VictoryChart>
+        <div style={{ padding: '50px', margin: '50px' }}>
+          <VictoryChart theme={VictoryTheme.material} domainPadding={30} padding={60}>
+            <VictoryAxis />
+            <VictoryAxis
+              dependentAxis
+              tickValues={[50000, 75000, 100000, 125000, 150000]}
+              tickFormat={(tick) => {
+                return `$${tick}`
+              }}
+            />
+
+            <VictoryStack colorScale="heatmap">
+              <VictoryBar
+                data={schoolsData}
+                x="name"
+                y={(datum) => datum.school[0].salaryRange[1] - datum.school[0].salaryRange[0]}
+              />
+              <VictoryBar
+                data={schoolsData}
+                x="name"
+                y={(datum) => datum.school[1].salaryRange[1] - datum.school[1].salaryRange[0]}
+              />
+              <VictoryBar
+                data={schoolsData}
+                x="name"
+                y={(datum) => datum.school[2].salaryRange[1] - datum.school[2].salaryRange[0]}
+              />
+              <VictoryBar
+                data={schoolsData}
+                x="name"
+                y={(datum) => datum.school[3].salaryRange[1] - datum.school[3].salaryRange[0]}
+              />
+              <VictoryBar
+                data={schoolsData}
+                x="name"
+                y={(datum) => datum.school[4].salaryRange[1] - datum.school[4].salaryRange[0]}
+              />
+              <VictoryBar
+                data={schoolsData}
+                x="name"
+                y={(datum) => datum.school[5].salaryRange[1] - datum.school[5].salaryRange[0]}
+              />
+            </VictoryStack>
+          </VictoryChart>
+        </div>
       </div>
     )
   }
