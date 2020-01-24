3991fcde2b165ba720e1aae73579cd8b5dfcb348
Avelina Kim <avelina@gmail.com>
2020-01-24 11:38:27 -0500

    style: add grommet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { Grommet, grommet } from 'grommet'
 import Routes from './Routes'
 import NavBar from './NavBar'
 
 const App = (props) => {
   return (
     <Router>
-      <NavBar />
-      <Routes />
+      <Grommet theme={grommet}>
+        <NavBar />
+        <Routes />
+      </Grommet>
     </Router>
   )
 }
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Box, Grommet, Header, Nav } from 'grommet'
 
 const NavBar = (props) => (
   <nav>
