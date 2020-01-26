1e4f8dc5d85df8b83d07d9a732f49d2d3d596eb5
Avelina Kim <avelina@gmail.com>
2020-01-25 22:12:52 -0500

    refactor: scrape PDFs with promises and start extracting data

--- web-scraper/index.js
+++ web-scraper/index.js
@@ -2,6 +2,8 @@ const fetch = require('node-fetch')
 const cheerio = require('cheerio')
 const https = require('https')
 const fs = require('fs')
+const tabula = require('tabula-js')
+const { exec } = require('child_process')
 
 console.log('runnin')
 
@@ -36,20 +38,88 @@ const findLinks = async () => {
   }
 }
 
-const download = async () => {
+const downloadPDFs = async () => {
   try {
     const links = await findLinks()
-    links.forEach((link, i) => {
+    const paths = []
+    for (let link of links) {
       const linkArr = link.split('/')
       const name = linkArr[linkArr.length - 1]
       if (!name.includes('Not Audited')) {
-        const file = fs.createWriteStream(`./PDFs/${name}`)
-        const request = https.get(link, (response) => response.pipe(file))
+        await downloadFile(link, `./PDFs/${name}`)
+        paths.push(`./PDFs/${name}`)
       }
-    })
+    }
+    return paths
   } catch (error) {
     console.log('Error:', error)
   }
 }
 
-download()
+const downloadFile = async (link, path) => {
+  const res = await fetch(link)
+  const fileStream = fs.createWriteStream(path)
+  return new Promise((resolve, reject) => {
+    res.body.pipe(fileStream)
+    res.body.on('error', (err) => {
+      reject(err)
+    })
+    fileStream.on('finish', function() {
+      resolve()
+    })
+  })
+}
+
+const asyncExec = async (cmd) => {
+  return new Promise((resolve, reject) => {
+    const child = exec(cmd, (err, stdout, stderr) => {
+      if (err) reject(err)
+      else {
+        console.log('stderr', stderr)
+        resolve(stdout)
+      }
+    })
+  })
+}
+
+const formatSchool = (school) => {
+  let name = school.script
+  let start = name
+}
+
+const scrapePDFs = async () => {
+  // let sources = await downloadPDFs()
+  const script = './PDFs/Hack Reactor San Francisco H1 2018.pdf'
+  const cmd = 'java -jar ./node_modules/tabula-js/lib/tabula-java.jar "' + script + '" --format JSON --pages 2'
+  let rawJSON = await asyncExec(cmd)
+  const JSONObj = await JSON.parse(rawJSON)
+
+  let data
+  // console.log(JSONObj[1])
+  if (JSONObj.length > 1) {
+    data = JSONObj[1].data
+  } else data = JSONObj[0].data
+
+  const schoolName = data[1][1].text
+  const location = data[2][1].text
+  const median = data[26][2].text
+  const salaryData = []
+
+  for (let i = 27; i < 33; i++) {
+    const dataArr = data[i]
+    let salaryRange = dataArr[0].text
+    let percentage = dataArr[1].text
+    salaryData.push({ salaryRange, percentage })
+  }
+  // const school = { script, median, salaryData }
+  console.log(location)
+  console.log(salaryData)
+  // console.log('Child', child)
+}
+
+scrapePDFs()
+
+// java -jar ./node_modules/tabula-js/lib/tabula-java.jar ./PDFs/Hack\ Reactor\ San\ Francisco\ H1\ 2018.pdf --format JSON --pages 2 >> example.json
+
+const filePaths = fs.readdirSync('./filtered-PDFs').map((name) => `./filtered-PDFs/${name}`)
+console.log(filePaths)
